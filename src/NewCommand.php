9d416422cf28e77f7e58dc41e70c26092503d3c1
Dustin Woodard <dustin.woodard@gmail.com>
2024-01-25 18:30:25 -0700

    Add laradock and composer packages

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -41,41 +41,19 @@ class NewCommand extends Command
       ->setName('new')
       ->setDescription('Create a new Laravel application')
       ->addArgument('name', InputArgument::REQUIRED)
-      ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
-      ->addOption('git', null, InputOption::VALUE_NONE, 'Initialize a Git repository')
-      ->addOption('branch', null, InputOption::VALUE_REQUIRED, 'The branch that should be created for a new repository', $this->defaultBranch())
-      ->addOption('github', null, InputOption::VALUE_OPTIONAL, 'Create a new repository on GitHub', false)
-      ->addOption('organization', null, InputOption::VALUE_REQUIRED, 'The GitHub organization to create the new repository for')
-      ->addOption('stack', null, InputOption::VALUE_OPTIONAL, 'The Breeze / Jetstream stack that should be installed')
-      ->addOption('breeze', null, InputOption::VALUE_NONE, 'Installs the Laravel Breeze scaffolding')
-      ->addOption('jet', null, InputOption::VALUE_NONE, 'Installs the Laravel Jetstream scaffolding')
-      ->addOption('dark', null, InputOption::VALUE_NONE, 'Indicate whether Breeze or Jetstream should be scaffolded with dark mode support')
-      ->addOption('typescript', null, InputOption::VALUE_NONE, 'Indicate whether Breeze should be scaffolded with TypeScript support (Experimental)')
-      ->addOption('ssr', null, InputOption::VALUE_NONE, 'Indicate whether Breeze or Jetstream should be scaffolded with Inertia SSR support')
-      ->addOption('api', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with API support')
-      ->addOption('teams', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with team support')
-      ->addOption('verification', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with email verification support')
       ->addOption('pest', null, InputOption::VALUE_NONE, 'Installs the Pest testing framework')
       ->addOption('phpunit', null, InputOption::VALUE_NONE, 'Installs the PHPUnit testing framework')
-      ->addOption('prompt-breeze', null, InputOption::VALUE_NONE, 'Issues a prompt to determine if Breeze should be installed (Deprecated)')
-      ->addOption('prompt-jetstream', null, InputOption::VALUE_NONE, 'Issues a prompt to determine if Jetstream should be installed (Deprecated)')
       ->addOption('force', 'f', InputOption::VALUE_NONE, 'Forces install even if the directory already exists')
       // Add custom options below...
 
       //add laradock
-      ->addOption('laradock', null, InputOption::VALUE_NONE, 'Installs the Laradock scaffolding (in project)')
+      ->addOption('laradock', null, InputOption::VALUE_NONE, 'Installs the Laradock scaffolding (in project)', true)
 
       //composer packages to add
-      // pwa
       ->addOption('laravelpwa', null, InputOption::VALUE_NONE, 'Installs the PWA scaffolding (https://github.com/silviolleite/laravel-pwa)')
-      //spatie/laravel-schemaless-attributes
       ->addOption('laravel-schemaless-attributes', null, InputOption::VALUE_NONE, 'Installs the spatie/laravel-schemaless-attributes scaffolding (https://github.com/spatie/laravel-schemaless-attributes)')
-      //spatie/laravel-permission
       ->addOption('laravel-permission', null, InputOption::VALUE_NONE, 'Installs the spatie/laravel-permission scaffolding (https://github.com/spatie/laravel-permission)')
-      //spatie/laravel-medialibrary
-      ->addOption('laravel-medialibrary', null, InputOption::VALUE_NONE, 'Installs the spatie/laravel-medialibrary scaffolding (https://github.com/spatie/laravel-medialibrary)')
-      //spatie/laravel-tags
-    ;
+      ->addOption('laravel-medialibrary', null, InputOption::VALUE_NONE, 'Installs the spatie/laravel-medialibrary scaffolding (https://github.com/spatie/laravel-medialibrary)');
   }
 
   /**
