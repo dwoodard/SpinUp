3f19c8dba165386d12679fc2433af9c8e2694b19
Dustin Woodard <dustin.woodard@gmail.com>
2024-02-17 19:39:35 -0700

    add motion vue plugin

---
+++ stubs/resources/js/Components/Example.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+
+<button>
+        <div
+         v-motion
+          :initial="{ scale: 1 }"
+           :enter="{
+            scale: 1.2,
+             transition: {
+            repeat: Infinity,
+            repeatType: 'mirror'
+             } }" >
+            hey there 👋
+        </div>
+    </button>
+</template>
+
+<style scoped>
+</style>
